eb94dcabe24f976e8f9586db262720e83d5045b2
zxy <zhao-xiao-yun@126.com>
2022-11-06 18:16:29 +0800

    新增属性name age height

--- TestModule01/src/com/sunline/Student.java
+++ TestModule01/src/com/sunline/Student.java
@@ -6,5 +6,19 @@ package com.sunline;
  * @description: com.sunline
  * @version: 1.0
  */
+
+/*
+* idea集成git操作
+*   1. 创建project
+*   2. settings - version control - 添加git bin 下 git.exe
+*   3. 初始化项目目录 作为本地仓库 VCS - IMPORT INTO VERSION - CREATE git...
+*   4. 创建模块-类 右键-git - add 提交到暂存区 - commit - 本地库
+*       console：git 控制台输出
+*       log：相当于git log
+*
+* */
 public class Student {
+    private String name;
+    private int age;
+    private double height;
 }
