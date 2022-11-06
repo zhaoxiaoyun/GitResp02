75d745006aaf0d95f6f7c52c1401a2d0b01965cf
zxy <zhao-xiao-yun@126.com>
2022-11-06 18:32:08 +0800

    update..

--- TestModule01/src/com/sunline/Student.java
+++ TestModule01/src/com/sunline/Student.java
@@ -8,14 +8,16 @@ package com.sunline;
  */
 
 /*
-* idea集成git操作
+*1.  idea集成git操作
 *   1. 创建project
 *   2. settings - version control - 添加git bin 下 git.exe
 *   3. 初始化项目目录 作为本地仓库 VCS - IMPORT INTO VERSION - CREATE git...
 *   4. 创建模块-类 右键-git - add 提交到暂存区 - commit - 本地库
 *       console：git 控制台输出
 *       log：相当于git log
-*
+*2. 与远程库关联
+*   pull: git pull 远程库地址 分支 --allow-unrelated-histories
+*   push: git push -u 远程库地址 分支 -f
 * */
 public class Student {
     private String name;
