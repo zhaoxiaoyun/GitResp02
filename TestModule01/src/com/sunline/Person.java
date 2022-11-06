70ee898a34216ce685fb0ad6e57070909ae5ffde
zxy <zhao-xiao-yun@126.com>
2022-11-06 18:41:45 +0800

    创建person类 新增属性name age

---
+++ TestModule01/src/com/sunline/Person.java
@@ -0,0 +1,12 @@
+package com.sunline;
+
+/**
+ * @author: zxy
+ * @date: 2022/11/6 - 18:38
+ * @description: com.sunline
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+    private int age;
+}
